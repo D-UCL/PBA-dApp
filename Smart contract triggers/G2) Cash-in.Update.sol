304f91270e8c300433890d248059f4b7b794ba09
D-UCL <60363671+D-UCL@users.noreply.github.com>
2022-11-01 13:37:33 +0000

    Updated code

--- Smart contract triggers/G2) Cash-in.Update.sol
+++ Smart contract triggers/G2) Cash-in.Update.sol
@@ -1,7 +1,7 @@
 pragma solidity ^0.8.4;
 pragma experimental ABIEncoderV2;
 
-//Name: mPBA_update_3
+//Name: CashIn_MC_update_003
 //Description: 
 
 contract owned {
@@ -81,65 +81,71 @@ contract owned {
   }
 }
 
-interface ImainPBA {
+interface ICashInI {
   struct Data {
     uint256 A_Added;
-    string A_Milestone;
-    string A_Revision;
+    string A_Role;
+    uint256 A_ID;
+    string A_Contract;
+    uint256 A_Milestone;
+    uint256 A_Revision;
+    uint256 A_Start;
+    uint256 A_End;
     uint256 A_Planned;
     uint256 A_Actual;
-    string A_Schedule;
+    string A_CostCode;
     string A_Status;
-    uint256 A_From;
-    uint256 A_To;
+    uint256 A_PercentageComp;
+    uint256 A_DaysBehind;
+    uint256 A_Updated;
     address payable A_PBA;
   }
   function AcceptOwnership() external returns(bool);
   function AddPermission(address addr) external returns(bool);
   function Delete(address recordId) external returns(bool);
   function Exists(address recordId) external returns(bool);
-  function GetById(address recordId) external returns(uint256,ImainPBA.Data memory);
-  function GetByIndex(uint256 recordIndex) external returns(address,ImainPBA.Data memory);
+  function GetById(address recordId) external returns(uint256,ICashInI.Data memory);
+  function GetByIndex(uint256 recordIndex) external returns(address,ICashInI.Data memory);
   function GetLength() external returns(uint256);
   function GetPermission(uint256 index) external returns(address);
   function GetPermissionListLength() external returns(uint256);
   function HasPermission(address sender) external returns(bool);
   function IdList(uint256 ) external returns(address);
-  function Insert(ImainPBA.Data calldata) external returns(bool);
+  function Insert(ICashInI.Data calldata) external returns(bool);
   function Name() external returns(string memory);
   function RemovePermission(address addr) external returns(bool);
-  function Table(address ) external returns(ImainPBA.Data memory,uint256);
+  function Table(address ) external returns(ICashInI.Data memory,uint256);
   function TransferOwnership(address _newOwner) external returns(bool);
-  function Update(address recordId, ImainPBA.Data calldata) external returns(bool);
+  function Update(address recordId, ICashInI.Data calldata) external returns(bool);
   function newOwner() external returns(address);
   function owner() external returns(address);
   function permissionedList(uint256 ) external returns(address);
 }
-interface IMC {
+interface IMainContractor {
   struct Data {
     uint256 A_Added;
     string A_Role;
     string A_ID;
     string A_Contract;
-    address payable A_Address;
+    address payable A_Wallet;
   }
   function AcceptOwnership() external returns(bool);
   function AddPermission(address addr) external returns(bool);
   function Delete(address recordId) external returns(bool);
   function Exists(address recordId) external returns(bool);
-  function GetById(address recordId) external returns(uint256,IMC.Data memory);
-  function GetByIndex(uint256 recordIndex) external returns(address,IMC.Data memory);
+  function GetById(address recordId) external returns(uint256,IMainContractor.Data memory);
+  function GetByIndex(uint256 recordIndex) external returns(address,IMainContractor.Data memory);
   function GetLength() external returns(uint256);
   function GetPermission(uint256 index) external returns(address);
   function GetPermissionListLength() external returns(uint256);
   function HasPermission(address sender) external returns(bool);
   function IdList(uint256 ) external returns(address);
-  function Insert(IMC.Data calldata) external returns(bool);
+  function Insert(IMainContractor.Data calldata) external returns(bool);
   function Name() external returns(string memory);
   function RemovePermission(address addr) external returns(bool);
-  function Table(address ) external returns(IMC.Data memory,uint256);
+  function Table(address ) external returns(IMainContractor.Data memory,uint256);
   function TransferOwnership(address _newOwner) external returns(bool);
-  function Update(address recordId, IMC.Data calldata) external returns(bool);
+  function Update(address recordId, IMainContractor.Data calldata) external returns(bool);
   function newOwner() external returns(address);
   function owner() external returns(address);
   function permissionedList(uint256 ) external returns(address);
@@ -148,18 +154,18 @@ interface IMC {
 contract trigger is owned {
   using SafeMath for uint;
 
-  address mainPBAAddress = 0x7fdaC39251B63EE8078292E2B612B7fbb68DaA45;
-  address MCAddress = 0xBE88b614e8cDE058e7556c4a2F2C4304F26F30ba;
+  address CashInIAddress = 0x8A17A1fF265734D6ddF240f070a5090B8720F130;
+  address MainContractorAddress = 0xdC032b81464e64b3592335DF8185799283dC23c7;
 
-  function invoke(address _recordId,ImainPBA.Data memory mainPBA_Data) public  returns(bool){
+  function invoke(address _recordId,ICashInI.Data memory CashInI_Data) public  returns(bool){
 
     //Instantiate Global Interfaces
-    ImainPBA mainPBA = ImainPBA(mainPBAAddress);
+    ICashInI CashInI = ICashInI(CashInIAddress);
 
     //Declare and Initialize Constant Interfaces
-    uint256 mainPBA_GetById_index;
-    ImainPBA.Data memory mainPBA_GetById_record;
-    (mainPBA_GetById_index,mainPBA_GetById_record) = mainPBA.GetById(_recordId);
+    uint256 CashInI_GetById_index;
+    ICashInI.Data memory CashInI_GetById_record;
+    (CashInI_GetById_index,CashInI_GetById_record) = CashInI.GetById(_recordId);
 
     //Required Payment Options
 
@@ -167,16 +173,20 @@ contract trigger is owned {
       Condition0(msg.sender);
 
     //Map Values to Action Interface
-    mainPBA_Data.A_Added = mainPBA_GetById_record.A_Added;
-    mainPBA_Data.A_Milestone = mainPBA_GetById_record.A_Milestone;
-    mainPBA_Data.A_Planned = mainPBA_GetById_record.A_Planned;
-    mainPBA_Data.A_Status = 'Updated';
-    mainPBA_Data.A_From = mainPBA_GetById_record.A_From;
-    mainPBA_Data.A_To = mainPBA_GetById_record.A_To;
-    mainPBA_Data.A_PBA = mainPBA_GetById_record.A_PBA;
+    CashInI_Data.A_Added = CashInI_GetById_record.A_Added;
+    CashInI_Data.A_Role = CashInI_GetById_record.A_Role;
+    CashInI_Data.A_ID = CashInI_GetById_record.A_ID;
+    CashInI_Data.A_Contract = CashInI_GetById_record.A_Contract;
+    CashInI_Data.A_Milestone = CashInI_GetById_record.A_Milestone;
+    CashInI_Data.A_Actual = CashInI_GetById_record.A_Actual;
+    CashInI_Data.A_Status = 'Updated';
+    CashInI_Data.A_PercentageComp = CashInI_GetById_record.A_PercentageComp;
+    CashInI_Data.A_Updated = block.timestamp * 1000;
+    CashInI_Data.A_PBA = CashInI_GetById_record.A_PBA;
+    CashInI_Data.A_Revision = CashInI_GetById_record.A_Revision.add(1);
 
     //Execute Action
-    require(mainPBA.Update(_recordId,mainPBA_Data));
+    require(CashInI.Update(_recordId,CashInI_Data));
 
     //Return Success
     return true;
@@ -184,16 +194,16 @@ contract trigger is owned {
 
   //Condition Functions
   function Condition0(address _msgSenderBase) private  {
-        IMC MC = IMC(MCAddress);
+        IMainContractor MainContractor = IMainContractor(MainContractorAddress);
         bool contains = false;
 
-        for(uint x = 0; x < MC.GetLength(); x++){
+        for(uint x = 0; x < MainContractor.GetLength(); x++){
 
-          address MC_GetByIndex_recordId;
-          IMC.Data memory MC_GetByIndex_record;
-          (MC_GetByIndex_recordId,MC_GetByIndex_record) = MC.GetByIndex(x);
+          address MainContractor_GetByIndex_recordId;
+          IMainContractor.Data memory MainContractor_GetByIndex_record;
+          (MainContractor_GetByIndex_recordId,MainContractor_GetByIndex_record) = MainContractor.GetByIndex(x);
 
-            if(_msgSenderBase == MC_GetByIndex_record.A_Address){
+            if(_msgSenderBase == MainContractor_GetByIndex_record.A_Wallet){
               contains = true;
               break;
             }
